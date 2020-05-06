80f7e0d9680955c073a86113d5e10d5bceaa682d
mj <hi.minjungkim@gmail.com>
2020-05-06 18:52:06 +0900

    Practice useTabs

--- src/LectureInput.js
+++ src/LectureInput.js
@@ -1,30 +1,42 @@
 import React, { useState, useEffect } from "react";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
+
+const useTabs = (initialTab, allTabs) => {
+  // if (!allTabs || !Array.isArray(allTabs)) {
+  //   return;
+  // }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
   };
-  return { value, onChange };
 };
 
 const LectureInput = () => {
-  //   const maxLength = (value) => value.length <= 10;
-  const maxLength = (value) => !value.includes("@");
-  const name = useInput("Ms.", maxLength);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className='InputPractice'>
-      <h1>Pratice useInput</h1>
-      <input className='Name' {...name} />
+      <h1>Pratice useTabs</h1>
+      {content.map((section, index) => (
+        <button
+          onClick={() => {
+            changeItem(index);
+          }}
+        >
+          {section.tab}
+        </button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
