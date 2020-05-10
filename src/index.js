080fea5df8e7236fb234de6b054b3ffc6c7179ee
howdy-mj <hi.minjungkim@gmail.com>
2020-05-10 17:13:56 +0900

    useTitle

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import "./index.css";
 import Popup from "./Popup/Popup";
 import InputPractice from "./InputPractice/InputPractice";
 import PlusMinus from "./PlusMinus/PlusMinus";
-import LectureInput from "./LectureInput";
-import LectureuseEffect from "./LectureuseEffect";
+import UseInput from "./UseInput";
+import UseEffect from "./UseEffect";
+import UseTitle from "./UseTitle";
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Popup /> */}
     {/* <InputPractice /> */}
     {/* <PlusMinus /> */}
-    {/* <LectureInput /> */}
-    <LectureuseEffect />
+    {/* <UseInput /> */}
+    {/* <UseEffect /> */}
+    <UseTitle />
   </React.StrictMode>,
   document.getElementById("root")
 );
